ab343e092b84600e7eb42b151acb706ee3e786be
Sirawhic Saiweaw <dragonbul6@gmail.com>
2020-06-27 15:54:03 +0700

    add function

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,17 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- funtion(x = matrix()) {
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function(){
+    x
+  }
+  
+  list(set = set,get = get)
 }
 
 
@@ -12,4 +21,38 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  if ((ncol(x) >= 4) | (nrow(x) >= 4)) {
+    stop('Matrix is not 2x2 or 3x3')
+  }
+  
+  dimm <- c(nrow(x),ncol(x))
+  inverse_value <- matrix()
+  if(dimm[1] == 2 && dimm[2] == 2){
+    
+    if(detx == 0){
+      stop('Determinant of matrix equals 0, no inverse exists')
+    }
+    
+    if(ncol(A) == 1){
+      stop("vector")
+    }
+
+    inverse_value <- solve(x)
+  }
+  if(dimm[1] == 3 && dimm[2] == 3){
+    detx <- det(x)
+    if (detx == 0) {
+      stop('singular matrix')
+    }
+    
+    ##find inverse
+    inverse_value <- solve(x)
+  
+    
+  }
+  
+  return(inverse_value)
+  
+  
 }
